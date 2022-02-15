51d1b4800ed54bbecddca7e3162936c36a801d5b
satinder <satinder@ecommercecanada.com>
2022-02-14 21:58:44 -0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase form 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCm6Ge2jMxgqmWE_MXehXdL4cSyjg3vv6I",
+  authDomain: "fir-20491.firebaseapp.com",
+  projectId: "fir-20491",
+  storageBucket: "fir-20491.appspot.com",
+  messagingSenderId: "609236487073",
+  appId: "1:609236487073:web:6c0cf2acb2570981d799e0",
+};
